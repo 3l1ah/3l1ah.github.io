79f9ed4472491f76a0ecd7fccdab2929b67fe93e
cz <me@czcs.xyz>
2025-02-02 14:14:44 -0500

    webfont

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
 	import '../app.css';
+
 	import namegames from '$lib/namegames.svg';
+	import discord from '$lib/discord.svg';
+
 	import Squares from '$lib/Squares.svelte';
 	let { children } = $props();
 </script>
 
 <div class="overscroll-none font-inconsolata text-white">
-	<div class="fixed top-0 left-0 bg-gradient-to-b from-black right-0 z-50 flex p-3">
+	<div class="fixed top-0 left-0 bg-gradient-to-b justify-between from-black right-0 z-50 flex p-3">
 		<img class="size-10" src={namegames} />
+		<div class="flex gap-2">
+			<a href="https://discord.gg/namegames"><img class="size-10" src={discord} /></a>
+		</div>
 	</div>
 	{@render children()}
 	<div class="border-t items-center gap-3 p-6 border-amber-600 mt-6 flex">
